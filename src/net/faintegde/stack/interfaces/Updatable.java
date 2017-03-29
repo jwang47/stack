ff645b355cf9b6c6ecf4a48ba6a0fc00d921e422
Jiahua Wang <jiahuawang7@gmail.com>
2017-03-28 17:17:10 -0700

    Fix typo in package name (part 2)

--- src/net/faintegde/stack/interfaces/Updatable.java
+++
@@ -1,8 +0,0 @@
-package net.faintegde.stack.interfaces;
-
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.SlickException;
-
-public abstract interface Updatable {
-  public abstract void update(GameContainer paramGameContainer, int paramInt) throws SlickException;
-}
--- src/net/faintegde/stack/test/ModTest.java
+++
@@ -1,8 +0,0 @@
-package net.faintegde.stack.test;
-
-
-public class ModTest {
-  public static void main(String[] args) {
-    System.out.println(-2);
-  }
-}
